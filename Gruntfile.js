053e68b0a1966c22c93aed0ad20f4c144595d123
cafedeaqua <cafedeaqua@users.noreply.github.com>
2015-01-07 01:33:58 +0900

    Add grunt configuration file

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'index.js']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('travis', 'jshint');
+
+};
+
+
